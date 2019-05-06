317752a4ca3c497d72f2fe37e0204b79e6f9b9e9
Xett <3954937+Xett@users.noreply.github.com>
2019-05-06 11:26:39 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,37 @@
-#!/bin/sh
-# Import variables
-SCRIPT=$(readlink -f "$0")
-PROJECT_DIRECTORY=$(dirname "$SCRIPT")
-cp -rl hooks .git
+project_directory()
+{
+   if [[ "$OSTYTPE" == "darwin"* ]]
+   then
+      TARGET_FILE=$0
+      cd `dirname $TARGET_FILE`
+      TARGET_FILE=`basename $TARGET_FILE`
+      # Iterate down a (possible) chain of symlinks
+      while [ -L "$TARGET_FILE" ]
+      do
+        TARGET_FILE=`readlink $TARGET_FILE`
+        cd `dirname $TARGET_FILE`
+        TARGET_FILE=`basename $TARGET_FILE`
+      done
+      PROJECT_DIRECTORY= echo `pwd -P` | echo $(sed -E 's/\/scripts//g') | sed -E 's/\/.git\/hooks//g'
+   else
+      SCRIPT=$(readlink -f "$0")
+      PROJECT_DIRECTORY= echo $(dirname "$SCRIPT") | echo $(sed -E 's/\/scripts//g') | sed -E 's/\/.git\/hooks//g'
+   fi
+   echo $PROJECT_DIRECTORY
+}
+PROJECT_DIRECTORY="$(project_directory)"
+install()
+{
+   if [[ "$OSTYPE" == "darwin"* ]]
+   then
+      cp -RL $PROJECT_DIRECTORY"/hooks" $PROJECT_DIRECTORY"/.git"
+   elif [[ "$OSTYPE" == "msys" ]]
+   then
+      cp -rl $PROJECT_DIRECTORY"/hooks" $PROJECT_DIRECTORY"/.git"
+   else
+      cp -rl $PROJECT_DIRECTORY"/hooks" $PROJECT_DIRECTORY"/.git"
+   fi
+}
 
 # Pause at end
 read -p "Press enter to continue"
